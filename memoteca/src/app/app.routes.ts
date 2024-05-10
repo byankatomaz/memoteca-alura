748585d834dab0e36fa6aaa1448082c0c19d8470
byankatomaz <byankatomaz@gmail.com>
2024-05-10 09:39:02 -0300

    update app.routes.ts

--- memoteca/src/app/app.routes.ts
+++ memoteca/src/app/app.routes.ts
@@ -1,3 +1,29 @@
 import { Routes } from '@angular/router';
+import { CriarPensamentoComponent } from './Components/pensamentos/criar-pensamento/criar-pensamento.component';
+import { ListarPensamentoComponent } from './Components/pensamentos/listar-pensamento/listar-pensamento.component';
+import { ExcluirPensamentoComponent } from './Components/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './Components/pensamentos/editar-pensamento/editar-pensamento.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'listarPensamento',
+        pathMatch: 'full'
+    },
+    {
+        path: 'criarPensamento',
+        component: CriarPensamentoComponent
+    },
+    {
+        path: 'listarPensamento',
+        component: ListarPensamentoComponent
+    },
+    {
+        path: 'pensamentos/excluirPensamento/:id',
+        component: ExcluirPensamentoComponent
+    },
+    {
+        path: 'pensamentos/editarPensamento/:id',
+        component: EditarPensamentoComponent
+    },
+];
