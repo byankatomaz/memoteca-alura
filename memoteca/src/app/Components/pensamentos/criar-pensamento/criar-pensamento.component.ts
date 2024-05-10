c2a83d22a463cb5e90c8fb957fa0acb55f50adce
byankatomaz <byankatomaz@gmail.com>
2024-05-10 09:38:24 -0300

    update criar-pensamento.component.ts

--- memoteca/src/app/Components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ memoteca/src/app/Components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,19 +1,38 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { ListarPensamentoComponent } from '../listar-pensamento/listar-pensamento.component';
+import { Pensamento } from '../pensamento/pensamento';
+import { PensamentoService } from '../pensamento.service';
 @Component({
   selector: 'app-criar-pensamento',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterLink, ListarPensamentoComponent],
   templateUrl: './criar-pensamento.component.html',
   styleUrl: './criar-pensamento.component.css'
 })
 export class CriarPensamentoComponent {
 
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: ''
+  constructor(
+    private service: PensamentoService,
+    private router: Router
+    ) { }
+
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: 'modelo1'
   }
+
+  criarPensamento() {
+    this.service.criar(this.pensamento).subscribe(() => {
+      this.router.navigate(['/listarPensamento'])
+    })
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+
 
 }
